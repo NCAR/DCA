2c22f684b1c7a03a1db5d86c39ddd0df2f25b1e1
Seth McGinnis <mcginnis@ucar.edu>
2022-08-12 17:53:20 -0600

    anomaly finished?

--- anomaly.R
+++ anomaly.R
@@ -26,7 +26,7 @@ source("names.R")
 ## = 19k slices ~= 53 years of data for 8 variables.
 
 ## Which is a lot if you don't need it all at once, so I'm going to
-## save data in separate files by month and location.
+## save data in separate files by location, month, & period.
 
 
 
@@ -45,13 +45,14 @@ source("names.R")
 
 
 
-## var-lon-lat dimensions, used for later steps
-cdim <- dim(baseline[[1]])
-cdnm <- dimnames(baseline[[1]])
+## empty var-lon-lat array for storing total data
+tdim <- dim(baseline[[1]])
+tdnm <- dimnames(baseline[[1]])
+tarr <- array(dim=tdim, dimnames=tdnm)
 
-## empty var-lat-lon-mon array for holding results
-mdim <- c(cdim, 12)
-mdnm <- c(cdnm, list(month=month.abb))
+## empty var-lat-lon-mon array for holding monthly data
+mdim <- c(tdim, 12)
+mdnm <- c(tdnm, list(month=month.abb))
 marr <- array(dim=mdim, dimnames=mdnm)
 
 ## for indexing
@@ -101,126 +102,60 @@ for(p in periods){
 
 ## save data to file & cleanup
 
-## ! TODO^^^
+outdir <- paste("data", gcm, "anom", sep='/')
+system(paste("mkdir -p", outdir))
 
+save(baseline, totclim, totanom, file=paste0(outdir,"/total.Rdata"))
 
 ### Now calculate bucket climatology & anomalies
 
 for(p in periods){
+    cat('--------',p,'\n')
+    period = p                                                      ## for save file
     for(x in px){
         for(y in py){
-            ## subset bucket dataframe for p / x / y 
-            with(bprec[[p]], xyind <<- px==x & py == y)
-            df.pxy <- bprec[[p]][xyind,]            
-            for(meth in methods){                
-                for(b in buckets){
-                    for(mon in month.abb){
-# HERE
-                    }
-#                    mbind <- df.pxy[[meth]] == b
-#                    bdays <- df.pxy[mbind, 1:4]
-
-                    ## subset ua array to bucket days for method(p,x,y)
-#                    uab <- ua[[p]][,,,mbind]
-                    
-                    
+            loc <- paste(x, y, sep='.')                             ## for save file
+            cat(loc,'')
+            for(mon in month.abb){
+                mm <- grep(mon, month.abb) |> sprintf(fmt="m%02d")  ## for save file
+                cat(mon,'')
                 
+                ## subset bucket dataframe for p / x / y / mon
+                with(bprec[[p]], sind <<- px==x & py == y & month == mon)
+                subdf <- bprec[[p]][sind,]            
+
+                ## storage for calculated results
+                climdata <- anomdata <- deltadata <- list()
+                
+                for(meth in methods){
+                    for(b in buckets){
+                        mbdates <- subdf$date[subdf[[meth]]==b]
+                        subua <- ua[[p]][,,,mbdates]
+
+                        id <- paste(meth, b, sep='.')
+                        
+                        climdata[[id]]  <- apply(subua, 1:3, mean)
+                        anomdata[[id]]  <- climdata[[id]] - baseline[[p]]
+                        deltadata[[id]] <- anomdata[[id]] - totanom[[p]][,,,m]
+                    }
+                }
+                
+                ## create output dataframes with array column for bucket values
+                clim <- anom <- delta <- expand.grid(method=methods, bucket=buckets)
+                clim$ua  <- climdata
+                anom$ua  <- anomdata
+                delta$ua <- deltadata
+                
+                ## save data
+                savedir <- paste(outdir, "sgp", loc, sep='/')
+                system(paste("mkdir -p", savedir))
+                filename <- paste(p, mm, loc, "Rdata", sep='.')
+                
+                save(gcm, loc, mon, period, clim, anom, delta,
+                     file=paste0(savedir, '/', filename))
             }
+            cat("\n")
         }
     }
 }
 
-
-
-
-
-
-#mua <- ua[[p]][,,,,m,]
-## subset to month, wrap YMD to single dimension, fix dimnames
-mua <- ua[[p]][,,,,m,,drop=FALSE] |>
-    wrap(list(1,2,3,4:6)) |>
-    setname(c(dimnamename(ua[[p]])[1:3],"date"),"ndn", 1:4)
-
-#mxybuck <- bprec[[p]][,px,py,,m,]
-## subset to loc (dropping dims), then to month & wrap YMD as above
-mxybuck <- bprec[[p]][,px,py,,,][,,m,,drop=FALSE] |>
-    wrap(list(1,2:4)) |>
-    setname(c("method","date"), "ndn", 1:2)
-
-## construct masks for each bucket (+"all" = union of the 3)
-bmask <- list()
-bmask$all <- mxybuck %in% buckets |> array(dim(mxybuck), dimnames(mxybuck))
-for(b in buckets){
-    bmask[[b]] <- (mxybuck == b) & bmask$all
-}
-
-### convert masks to arrays of index values
-#bindex <- lapply(bmask, asplit, MARGIN=1)|>
-#    rapply(which, classes="array", how="replace", arr.ind=TRUE)
-
-for(b in c("all", buckets){
-    for(mm in methods){
-
-#        mslice <- mua
-#        ## average over each month
-#        mslice <- mua[,,,iday,iyear] ## that's not what I want...
-        mslice <- mua[,,,bmask[[b]][mm,]]
-        climatology <- apply(mslice, 1:3, mean, na.rm=TRUE)
-        
- #       monthly <- apply(mslice, c("var","lon","lat","year"), mean, na.rm=TRUE)
-
-        
-        
-    monthly <- ...
-    
-    ## monthly[12*N]: average over each month
-    ## climatology[12]: average monthly across N years
-    ## monanom[12*N]: monthly minus climatology
-    ## avanom[12]: average monthly anomaly across N years
-    
-}
-
-
-#        }
-#    }
-#}
-
-## monthly averages: 
-
-monthly <- lapply(ua, apply, c("var","lat","lon"), mean, na.rm=TRUE)
-
-
-
-
-
-climatology <- function(x){
-    dnm <- dimnames(x) 
-    ndnm <- names(dnm)
-    
-    stopifnot(all(c("day","month","year") %in% ndnm) &&
-              dnm[["day"]] == sprintf("%02i",1:31) &&
-              dnm[["month"]] == month.abb)
-
-    marginx <- grep("day|month|year", ndnm, invert=TRUE, value=TRUE)
-    clim <- apply(x, marginx, mean, na.rm=TRUE)
-    y <- sweep(x, marginx, clim)
-
-    marginy <- grep("day|year", ndnm, invert=TRUE, value=TRUE)    
-    seas <- apply(y, marginy, mean, na.rm=TRUE)
-    anom <- sweep(y, marginy, seas)
-    list(clim=clim, seas=seas, anom=anom)
-}
-
-
-
-
-
-## Aggregate UA data by bprec[method, px, py, month] and difference from 
-
-## This gives 9x5x4x12 anomalies, but that's equivalent to a bit under
-## 6 years worth of daily data, so not bad.
-
-
-
-
-## Then difference then from climatology
