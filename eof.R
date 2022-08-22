384e5e4b8f5e6b0233c5ef5d44be20f9f02eaca9
Seth McGinnis <mcginnis@ucar.edu>
2022-08-22 17:04:37 -0600

    EOFs normalized, picking # PCs using elbow methods from nFactors

--- eof.R
+++ eof.R
@@ -1,7 +1,9 @@
 load("data/ua.Rdata")
+library(nFactors)
 #load("data/misc.Rdata")
 #load("data/buckets.Rdata")
 source("names.R")
+source("plotfun.R")
 
 
 ## PCA doesn't care about the spatial dimension of the data; you give
@@ -62,27 +64,47 @@ system.time(
     )
 
 
+
+## need to divide EOFs by corresponding eigenvalues and rearrange
+## dimensions for plotting with gridmap
+
+pcnm <- paste("PC", 1:nt)
+
+
+eof <- array(pca$x, dim=dim(uamon),
+             dimnames=c(dimnames(uamon)[-nd], list(pc=pcnm))) |>
+    sweep("pc", pca$sdev, "/") |> 
+    aperm(c("pc","var","lon","lat"))
+
+
+## visualize the first few EOFs
+
+
+nviz <- 8
+zr <- apply(eof[1:nviz,,,], 2, srange) |> as.data.frame() |> as.list()
+#gridmap(lon, lat, eof[1:nviz,,,]) 
+gridmap(lon, lat, eof[1:nviz,,,], cmaps=anomap, zlim=zr, main=paste("PCA",m,p))
+
+
+## How many PCs to keep?
+
 ## scree plot
 
 plot(pca$sd)
 plot(pca$sd, log='xy')
 
 
-## for the moment, let's keep the top 12 components (those with
-## eigenvalues greater than 3).  In actuality, looking at the log-log
-## plot, I think it's got 1/x-ish scaling from around component 20 to
-## component 100 or 150, so we should probably be keeping the first 20
-## or so.  Values above 2 gives us the first 25 components, so maybe
-## that's good.
+## from the nFactors package, which calculates elbow location using
+## several different methods
 
-eof <- array(pca$x, dim=dim(uamon),
-             dimnames=c(dimnames(uamon)[-nd], list(pc=NULL)))[,,,1:12]
+elbow <- nScree(pca$sdev)
 
-stop()
+dev.new()
+parsave <- par(no.readonly=TRUE)  ## plot.nScree messes up text colors
+plot(elbow, main=paste("scree test",m, p))
+par(parsave)
 
-source("plotfun.R")
-
-gridmap(lon, lat, eof)
+npc <- median(unlist(elbow$Components))
 
 
 ## rotate components to spread out variance
@@ -90,31 +112,4 @@ gridmap(lon, lat, eof)
 
 
 #}
-
-
-
-#for(N in 2^(2:14)){
-#    cat("\n",N,'\t')
-#    cat(system.time(pca <- prcomp(hist[1:N,], scale=TRUE)))
-#    plot(pca, main=N)
-#}
-
-## So it can do 2^14 = 16k x-y-var timeseries in 2 hours.  Which is
-## not so bad; it can probably do the whole set in 5-6 hours, then.
-
-system.time(pca <- prcomp(hist, scale=TRUE))
-
-
-## basically ALL of the variance is explained by the first mode, which
-## I'm guessing means that I need to remove the annual cycle.
-
-## probably also need to scale the entire field, rather than scaling
-## each gridcell+var separately (which is what the default scale=TRUE
-## does.
-
-## And if I split it into months, I'll drop the data size by a factor
-## of 12, which should speed it up to only about 3-4 minutes per month,
-## or about 45 minutes for the whole thing, rather than several hours.
-## It also removes the annual cycle in an en passant kind of way, so I
-## don't have to bother with doing it manually.
 
