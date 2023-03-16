67e38a91547311343b0211bf1093eaf498153405
Seth McGinnis <mcginnis@ucar.edu>
2023-03-16 13:35:13 -0600

    qdm, looping, done

--- statdown.R
+++ statdown.R
@@ -1,4 +1,5 @@
 ## Statistical downscaling methods for precip (other than SDSM)
+library(MBC)
 source("names.R")
 load("data/prec.SGP.Rdata")
 
@@ -9,18 +10,20 @@ load("data/prec.SGP.Rdata")
 
 pr <- list()
 
-pr$obs <- split(prec[prec$scen == "obs",], ~ locname + month)
+
+pr$obs <- split(prec[prec$scen == "obs",], ~ month + locname)
 pr$raw <- split(prec[prec$method =="raw",], drop=TRUE,
-                ~ gcm + locname + month)
+                ~ month + locname + gcm)
+## gcm last for proper ordering for mapply arg recycling
 
 
 ## shuffle data by month & year
-dummy <- function(df, ..., seed=222){
+dummy <- function(mdf, ..., seed=222){
     set.seed(seed)
-    result <- df
-    result$prec <- split(df$prec, df$year) |>
+    result <- mdf
+    result$prec <- split(mdf$prec, mdf$year) |>
         lapply(sample) |>
-        unsplit(df$year)
+        unsplit(mdf$year)
     result$method <- "dummy"
     return(result)
 }
@@ -61,16 +64,66 @@ simple <- function(mdf, odf){
     return(result)
 }
 
+## using Cannon's QDM instead of KDDM to match NA-CORDEX BC
 
-## Some methods need other variables, either surface or UA
-## So probably need to run the above extraction with a different set of
-## inputs and variables
-## 
-## kddm: by month & loc
-## 
-## MBCn: by month & loc (needs other surface vars)
-## 
-## GLM: by month & loc (needs UA vars for this loc)
-## ? https://github.com/SantanderMetGroup/downscaleR
-## Note: this is a perfect-prog downscaling method
+qdm <- function(mdf, odf){
+    obs <- odf$prec
+    cur <- mdf[mdf$scen == "hist","prec"]
+    fut <- mdf[mdf$scen != "hist","prec"]
 
+    bc <- QDM(obs,cur,fut, ratio=TRUE, trace=0.254)
+
+    result <- mdf
+    result[result$scen == "hist","prec"] <- bc$mhat.c
+    result[result$scen != "hist","prec"] <- bc$mhat.p
+    result$method <- "QDM"
+    return(result)
+}
+
+## (Dropping mBCN and GLM as out of scope because time)
+
+## And now we apply it to all of the data!
+
+## This could be done with chained apply fn's, but looping lets us
+## track progress
+
+## method ordering for viz/analysis purposes
+
+methods <- c("qdm", "simple", "dummy")
+
+for(method in methods){
+    pr[[method]] <- list()
+    cat("\n=====\n", method, "\n=====\n")
+    for(gcm in levels(prec$gcm)[-4]){
+        cat("\n-----\n", gcm, "\n-----\n")
+        for(loc in unique(prec$locname)){
+            cat(loc,": ")
+            for(month in 1:12){
+                cat(month, " ")
+                oid <- paste(month, loc, sep='.')
+                mid <- paste(oid, gcm, sep='.')                
+                pr[[method]][[mid]] <- do.call(method,
+                                               list(mdf=pr$raw[[mid]],
+                                                    odf=pr$obs[[oid]]))
+            }
+            cat("\n")
+        }
+    }
+}
+
+sdprec <- lapply(pr[methods], do.call, what=rbind) |>
+    do.call(what=rbind)
+
+## need to add levels to method factor, want to keep obs as last element
+
+dlev <- levels(prec$method)
+
+newlevs <- c(head(dlev, -1), "SDSM", methods, tail(dlev, 1))
+
+prec$method   <- factor(  prec$method, lev=newlevs)
+sdprec$method <- factor(sdprec$method, lev=newlevs)
+
+prec <- rbind(prec, sdprec)
+
+
+save(prec, file="data/prec.SGP.all.Rdata")
