d48d12081f7134332655ab0525f69137e6db0b78
Seth McGinnis <mcginnis@ucar.edu>
2023-03-29 14:15:19 -0600

    add buckets, trace, theta

--- misc.R
+++ misc.R
@@ -1,19 +1,19 @@
 ## generate miscellaneous useful helper variables
 
-load("data/prec.SGP.all.Rdata")
+load("data/buckets.Rdata")
 
 ## move obs to beginning for plotting convenience
 shift <- function(x){c(tail(x,1), head(x,-1))}
 
-gcms <- c("ERAI", "HadGEM", "MPI", "GFDL")
-methods <- shift(levels(prec$method))
+gcms <- c("ERAI", "HadGEM", "MPI", "GFDL")  ## ECS ordering
+methods <- shift(levels(bstat$method))
 dynmethods <- c("raw","RegCM4","WRF")
-scen <- shift(levels(prec$scen))
+scen <- shift(levels(bstat$scen))
 
-locs <- unique(prec$loc)
-plon <- unique(prec$lon)
-plat <- unique(prec$lat)
+locs <- unique(bstat$loc)
+plon <- unique(bstat$lon)
+plat <- unique(bstat$lat)
 
-save(file="data/rdata/misc.Rdata",
+save(file="data/rdata/misc.Rdata", buckets, trace, theta,
      gcms, methods, dynmethods, scen, locs, plon, plat)
 
