119fde6e4a16497890f235fed539391b2f294960
Seth McGinnis <mcginnis@ucar.edu>
2023-03-31 19:36:03 -0600

    barplot updates

--- barplots.R
+++ barplots.R
@@ -1,6 +1,6 @@
 source("names.R")
 load("data/buckets.Rdata")
-load("data/misc.Rdata")
+load("data/rdata/misc.Rdata")
 load("plot/cmaps.Rdata")
 
 outdir <- "plot/bucket"
@@ -20,81 +20,105 @@ gcms <- c("HadGEM","MPI","GFDL")  ## ordered by ECS
 ## overlay them.
 
 
-for(loc in unique(bstat$locname)){
-    for(mon in 1:12) {
+for(loc in "SGP-98-36"){
+    #for(loc in unique(bstat$locname)){
+    for(mon in 5){
+        #    for(mon in 1:12) {
 
         month <- month.abb[mon]
 
-        outname <- paste("bucket-stackbar", mnum[mon], loc, sep='.')
-        outfile <- paste0(outdir, '/', outname, ".png")
-        png(outfile, units="in", res=120, height=9, width=8)
+        for(ptype in c("change", "grouped")){
 
-#        dev.new(height=9,width=8)
+            outname <- paste("bucket","bar", ptype, mnum[mon], loc, sep='.')
+            outfile <- paste0(outdir, '/', outname, ".png")
+            png(outfile, units="in", res=120, height=9, width=8)
+            
+#            dev.new(height=9,width=8)
 
-        par(mar=c(1,4,2,0), mfrow=c(length(gcms),1), oma=c(5,0,5,2), cex.axis=1.2)
+            par(mar=c(1,4,2,0), mfrow=c(length(gcms),1), oma=c(5,0,5,2),
+                cex.axis=1.2)
 
-        for(GCM in gcms){
-            pbs <- subset(bstat, month==mon & locname==loc & gcm %in% c(GCM,"ERAI"))
+            for(GCM in gcms){
+                pbs <- subset(bstat, month==mon & locname==loc &
+                                     gcm %in% c(GCM,"ERAI"))
 
-            ## create NA array, fill in appropriate slots in 3 copies
-            pctarr <- array(dim=c(3, 1+3*nm), dimnames=list(bucket=buckets, method=NULL))
+                ## create NA array, fill in appropriate slots in 3 copies
+                pctarr <- array(dim=c(3, 1+3*nm),
+                                dimnames=list(bucket=buckets, method=NULL))
 
-            inject <- function(x,a,i){ a[,i] <- x; return(a)}
-            gsbar <- list(hist=pctarr, rcp85=pctarr, obs=pctarr)
-            slots <- list(hist=(1:nm)*3, rcp85=(1:nm)*3+1, obs=1)
+                inject <- function(x,a,i){ a[,i] <- x; return(a)}
+                gsbar <- list(hist=pctarr, rcp85=pctarr, obs=pctarr)
+                if(ptype == "change"){
+                    slots <- list(hist=(1:nm)*3, rcp85=(1:nm)*3+1, obs=1)
+                } else {
+                    slots <- list(hist=(1:nm)+2, rcp85=(1:nm)+3+nm, obs=1)
+                }
 
-            gsbar <- split(pbs[,c("scen","method","bucket","pct")], ~scen) |>
-                lapply(reshape, direction="wide", drop="scen",
-                       idvar="bucket", timevar="method") |>
-                lapply('[', -1) |>
-                lapply(as.matrix) |>
-                lapply(setname, buckets, "row") |>
-                mapply(FUN=inject, a=gsbar, i=slots, SIMPLIFY=FALSE)
+                gsbar <- split(pbs[,c("scen","method","bucket","pct")],~scen) |>
+                    lapply(reshape, direction="wide", drop="scen",
+                           idvar="bucket", timevar="method") |>
+                    lapply('[', -1) |>
+                    lapply(as.matrix) |>
+                    lapply(setname, buckets, "row") |>
+                    mapply(FUN=inject, a=gsbar, i=slots, SIMPLIFY=FALSE)
 
+                w <- 3
+                if(ptype == "change"){
+                    barwidth <- c(w, rep(c(1,w,w), nm))
+                } else {
+                    barwidth <- c(w, 1, rep(w, nm), 1, rep(w, nm), rep(0, nm-3))
+                }
+                
+                ## blank plot w/ axes
+                bpx <- barplot(pctarr, ylim=c(0,100), width=barwidth, xaxt='n')
+                if(GCM == tail(gcms,1)){
+                    if(ptype == "change"){
+                        ax <- c(bpx[1], (bpx[1:nm*3] + bpx[1:nm*3 + 1])/2)
+                        axis(side=1, las=3, labels=c("obs",methods), at=ax)
+                    } else {
+                        axis(side=1, las=3, labels=c(methods,methods,"obs"),
+                             at = unlist(sapply(slots, \(x){bpx[x]})))
+                    }
+                }
+                mtext("%", side=2, line=2)
+                mtext(GCM, side=4, line=0)
+                
+                ## 3 overlaid barplots per scenario
+                for(s in names(gsbar)){
+                    barplot(gsbar[[s]], col=bucketmap[[s]], axes=FALSE,
+                            add=TRUE, width=barwidth)
+                }
 
-            ## labels for bars (with blanks to get spacing right)
-            mnm <- rep("", ncol(pctarr))
-            mnm[1] <- "obs"
-            mnm[3*1:nm] <- methods
+                ## lines on top for obs & raw hist gcm
+                ## use segments b/c for some reason abline extends too far right
+                barx <- tail(bpx,1) + diff(tail(bpx,2)) * 0.6 |> rep(4)
+                obsy <- subset(pbs, scen=="obs")$pct   |> cumsum() |> head(-1)
+                rawy <- subset(pbs, method=="raw" & scen=="hist")$pct |>
+                                                                cumsum() |>
+                                                                head(-1)
 
-            barwidth <- c(3,1,3)
-
-            ## blank plot w/ axes
-            bpx <- barplot(pctarr, ylim=c(0,100), width=barwidth, xaxt='n')
-            if(GCM == tail(gcms,1)){
-                ax <- c(bpx[1], (bpx[1:nm*3] + bpx[1:nm*3 + 1])/2)
-                axis(side=1, las=3, labels=c("obs",methods), at=ax)
+                segments(rep(0,4), c(obsy, rawy), barx, c(obsy,rawy),
+                         col='red', lty=c("solid","solid","dotted","dotted"))
             }
-            mtext("%", side=2, line=2)
-            mtext(GCM, side=4, line=0)
 
-            ## use segments b/c for some reason abline extends too far right
-            barx <- tail(bpx,1) + diff(tail(bpx,2)) * 0.6 |> rep(4)
-            obsy <- subset(pbs, scen=="obs")$pct   |> cumsum() |> head(-1)
-            rawy <- subset(pbs, method=="raw" & scen=="hist")$pct |> cumsum() |> head(-1)
+            mtext(paste(month, "wet/dry days, ", loc), side=3, outer=TRUE,
+                  las=1, cex=1.5, line=0.5)
 
-            segments(rep(0,4), c(obsy, rawy), barx, c(obsy,rawy),
-                     col="gray", lty=c("solid","solid","dotted","dotted"), lwd=2)
-
-            ## 3 overlaid barplots per scenario
-            for(s in names(gsbar)){
-                barplot(gsbar[[s]], col=bucketmap[[s]], axes=FALSE, add=TRUE, width=barwidth)
-            }    
+ 
+            ## little 3x3 legend
+            
+            reset <- par(new=TRUE, fig=c(85,100,92,100)/100, mar=c(1,1,0,0),
+                         oma=c(0,0,1,2), las=1)
+            legarr <-array(1:9, dim=c(3,3),
+                           dimnames=list(names(bucketmap), buckets))
+            image(1:3, 1:3, t(legarr), col=unlist(bucketmap),
+                  axes=FALSE, ann=FALSE)
+            mtext(side=2, buckets, at=1:3, adj=1, line=0.5, cex=2/3)
+            mtext(side=1, names(bucketmap), at=1:3, cex=2/3)
+            par(reset)
+            
+            dev.off()
         }
-
-        mtext(paste(month, "wet/dry days, ", loc), side=3, outer=TRUE, las=1, cex=1.5, line=0.5)
-
- 
-        ## little 3x3 legend
- 
-        reset <- par(new=TRUE, fig=c(85,100,92,100)/100, mar=c(1,1,0,0), oma=c(0,0,1,2))
-        legarr <-array(1:9, dim=c(3,3), dimnames=list(names(bucketmap),buckets))
-        image(1:3, 1:3, legarr, col=unlist(bucketmap), axes=FALSE, ann=FALSE)
-        mtext(side=1, buckets, at=1:3, las=1, cex=2/3)
-        mtext(side=2, names(bucketmap), at=1:3, las=2, cex=2/3, adj=1, line=0.5)
-        par(reset)
-
-        dev.off()
     }
 }
 
