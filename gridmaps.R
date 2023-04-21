74c49d87f2741e5ad83c0475a9762ba5395f0ff8
Seth McGinnis <mcginnis@ucar.edu>
2023-04-21 15:22:51 -0600

    moved calcS & calcA to new file for use elsewhere

--- gridmaps.R
+++ gridmaps.R
@@ -2,6 +2,7 @@
 
 library(abind)
 
+source("calcfun.R")
 source("plotfun.R")
 source("names.R")
 source("util.R")
@@ -23,26 +24,6 @@ system(paste("mkdir -p", plotdir))
 xr <- c(-135,-55)
 yr <- c(20,60)
 bounds <- list(lon=xr, lat=yr)
-
-
-## calculate moisture advection
-## abind::asub lets us not care how many dimensions there are
-calcA <- function(arr, Dim=1){
-    u <- asub(arr, "U850", Dim)
-    v <- asub(arr, "V850", Dim)
-    q <- asub(arr, "Q850", Dim)
-    a <- q * sqrt(u^2 + v^2)
-    abind(arr, A850=a, along=Dim, use.dnns=TRUE)
-}
-
-## calculate wind speed
-calcS <- function(arr, Dim=1){
-    u <- asub(arr, "U250", Dim)
-    v <- asub(arr, "V250", Dim)
-    s <- sqrt(u^2 + v^2)
-    abind(arr, S250=s, along=Dim, use.dnns=TRUE)
-}
-
 
 
 infiles <- dir("data/anom", rec=TRUE, full=TRUE)
