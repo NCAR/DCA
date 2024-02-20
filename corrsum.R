d340b2b997fde1efca0a8061d33e8a78a53ae2f9
Seth McGinnis <mcginnis@ucar.edu>
2024-02-20 13:30:31 -0700

    checking if vector corr/err helps metrics; looks like yes

--- corrsum.R
+++ corrsum.R
@@ -41,6 +41,8 @@ anom <- lapply(infiles, listload) |>   ##listload from util.R
 xlim <- c(-113, -85)
 ylim <- c(28, 44)
 
+dlon <- lon[lon %within% xlim]
+dlat <- lat[lat %within% ylim]
 
 delta <- renest(anom$delta)$wet |> abind(along=0)
 delta <- delta[,, lon %within% xlim, lat %within% ylim]
@@ -86,7 +88,8 @@ write.csv(metrics, file="plot/corr.wet.delta.csv", row.names=FALSE)
 
 ###
 
-gcol <- c(HadGEM=2, MPI=4, GFDL=3) # red, blue, green
+#gcol <- c(HadGEM=2, MPI=4, GFDL=3) # red, blue, green
+gcol <- c(HadGEM=2, GFDL=3, MPI=4) # red, green, blue
 fade <- 0.3
 lcol <- adjustcolor(gcol, fade)
 names(lcol) <- names(gcol)
@@ -102,77 +105,77 @@ rsym <- c(raw=19,       # big dot #raw=20,       # small dot
           simple=124,   # |
           dummy=1)      # o
 
-png(file="plot/corr-mae.png", width=6, height=6, units="in", res=180)
-par(mfcol=c(3,3), mar=c(3.1, 3.1, 2, 1), mgp=c(1.5,0.5,0))
+# png(file="plot/corr-mae.png", width=6, height=6, units="in", res=180)
+# par(mfcol=c(3,3), mar=c(3.1, 3.1, 2, 1), mgp=c(1.5,0.5,0))
+# 
+# for (v in c("A850", "U850", "V850",
+#             "T700", "legend", "Z700",
+#             "S250", "U250", "V250")){
+#     if(v == "legend"){
+#         plot(c(0,1), c(0,1), pch=NA, ann=FALSE, axes=FALSE)
+#         legend("top", names(rsym), pch=rsym, ncol=2, cex=0.9)
+#         legend("bottomleft", names(gcol), fill=gcol, cex=0.8)
+#         legend("bottomright", lty=1:3, col=lleg, cex=0.8,
+#                c("dynamic", "spatial","point"))
+#     } else {
+#         m <- subset(metrics, vars==v & scen=="hist")
+#         plot(m$cor, m$mae, #xlim=c(-1,1), ylim=c(0,max(m$mae)),
+#          main=v, xlab="correlation", ylab="MAE",
+#          pch=rsym[m$method], col=gcol[m$gcm], cex=1.3)
+# 
+#         for(g in names(gcol)){
+#             mm <- subset(m, gcm==g & method != "dummy")
+#             segments(mm[1,"cor"], mm[1,"mae"], mm[-1,"cor"], mm[-1,"mae"],
+#                      col=lcol[g], lty=c(1,1,2,2,2,3,3))
+#         }
+#     }
+# }
+# dev.off()
 
-for (v in c("A850", "U850", "V850",
-            "T700", "legend", "Z700",
-            "S250", "U250", "V250")){
-    if(v == "legend"){
-        plot(c(0,1), c(0,1), pch=NA, ann=FALSE, axes=FALSE)
-        legend("top", names(rsym), pch=rsym, ncol=2, cex=0.9)
-        legend("bottomleft", names(gcol), fill=gcol, cex=0.8)
-        legend("bottomright", lty=1:3, col=lleg, cex=0.8,
-               c("dynamic", "spatial","point"))
-    } else {
-        m <- subset(metrics, vars==v & scen=="hist")
-        plot(m$cor, m$mae, #xlim=c(-1,1), ylim=c(0,max(m$mae)),
-         main=v, xlab="correlation", ylab="MAE",
-         pch=rsym[m$method], col=gcol[m$gcm], cex=1.3)
-
-        for(g in names(gcol)){
-            mm <- subset(m, gcm==g & method != "dummy")
-            segments(mm[1,"cor"], mm[1,"mae"], mm[-1,"cor"], mm[-1,"mae"],
-                     col=lcol[g], lty=c(1,1,2,2,2,3,3))
-        }
-    }
-}
-dev.off()
-
-
-## average correlations
-## convert MAE to rank product statistic
-
-calcrps <- function(df, colname){
-    df$rps <- 1/rank(df[[colname]])
-    return(df)
-}
-
-## geometric mean
-gmean <- function(x){exp(mean(log(x)))}
-
-
-## apply to only some columns
-capply <- function(X, MARGIN, FUN, COLS, ...){
-    X[,COLS] <- apply(X[,COLS], MARGIN, FUN, ...)
-    return(X)
-}
-    
-met2 <- subset(metrics, scen=="hist",
-               select=c("method","gcm", "vars","cor","mae")) |>
-    split(~ vars + gcm) |>
-    lapply(calcrps, "mae") |>
-    do.call(what=rbind)
-
-
-mcor <- split(met2, ~ method) |> lapply('[[',"cor") |> sapply(mean)
-mrps <- split(met2, ~ method) |> lapply('[[',"rps") |> sapply(gmean)
-
-m3 <- sqrt(mcor*mrps) |> sort(dec=TRUE)
-#? m3 <- (mcor+mrps)/2 |> sort(dec=TRUE)
-print(format(m3, digits=2))
-barplot(m3, las=2, main="metric")
-
-mgcor <- split(met2, ~ method + gcm) |> lapply('[[',"cor") |> sapply(mean)
-mgrps <- split(met2, ~ method + gcm) |> lapply('[[',"rps") |> sapply(gmean)
-mg3 <- (mgcor+mgrps)/2
-
-mg33 <- c(sort(mg3[1:9+9]), NA, sort(mg3[1:9]), NA, sort(mg3[1:9+18])) |> rev()
-
-par(las=2, mar=c(8,4,4,1))
-barplot(mg33, main="metric", col=c(rep(4,9), NA, rep(3,9), NA, rep(2,9)))
-
-print(format(mg33, digits=3))
+# 
+# ## average correlations
+# ## convert MAE to rank product statistic
+# 
+# calcrps <- function(df, colname){
+#     df$rps <- 1/rank(df[[colname]])
+#     return(df)
+# }
+# 
+# ## geometric mean
+# gmean <- function(x){exp(mean(log(x)))}
+# 
+# 
+# ## apply to only some columns
+# capply <- function(X, MARGIN, FUN, COLS, ...){
+#     X[,COLS] <- apply(X[,COLS], MARGIN, FUN, ...)
+#     return(X)
+# }
+#     
+# met2 <- subset(metrics, scen=="hist",
+#                select=c("method","gcm", "vars","cor","mae")) |>
+#     split(~ vars + gcm) |>
+#     lapply(calcrps, "mae") |>
+#     do.call(what=rbind)
+# 
+# 
+# mcor <- split(met2, ~ method) |> lapply('[[',"cor") |> sapply(mean)
+# mrps <- split(met2, ~ method) |> lapply('[[',"rps") |> sapply(gmean)
+# 
+# m3 <- sqrt(mcor*mrps) |> sort(dec=TRUE)
+# #? m3 <- (mcor+mrps)/2 |> sort(dec=TRUE)
+# print(format(m3, digits=2))
+# barplot(m3, las=2, main="metric")
+# 
+# mgcor <- split(met2, ~ method + gcm) |> lapply('[[',"cor") |> sapply(mean)
+# mgrps <- split(met2, ~ method + gcm) |> lapply('[[',"rps") |> sapply(gmean)
+# mg3 <- (mgcor+mgrps)/2
+# 
+# mg33 <- c(sort(mg3[1:9+9]), NA, sort(mg3[1:9]), NA, sort(mg3[1:9+18])) |> rev()
+# 
+# par(las=2, mar=c(8,4,4,1))
+# barplot(mg33, main="metric", col=c(rep(4,9), NA, rep(3,9), NA, rep(2,9)))
+# 
+# print(format(mg33, digits=3))
 
 ## This doesn't really seem right.
 
@@ -190,15 +193,15 @@ mvars <- vars[!vars %in% "Z500"]
 omean <- apply(obs, 1, mean, na.rm=TRUE)
 osd   <- apply(obs, 1, sd, na.rm=TRUE)
 
-dev.new()
-par(mfrow=c(3,3))
-for(v in mvars){
-    plot(density(obs[v,,], na.rm=TRUE), main=v)
-    rug(obs[v,,])
-    abline(v=omean[v])
-    abline(v=omean[v]+osd[v], lty=2)
-    abline(v=omean[v]-osd[v], lty=2)
-}
+# dev.new()
+# par(mfrow=c(3,3))
+# for(v in mvars){
+#     plot(density(obs[v,,], na.rm=TRUE), main=v)
+#     rug(obs[v,,])
+#     abline(v=omean[v])
+#     abline(v=omean[v]+osd[v], lty=2)
+#     abline(v=omean[v]-osd[v], lty=2)
+# }
 
 ## T700 is bimodal!  So that makes sigma weird.  Let's try interdecile
 ## range instead (10th to 90th percentiles).  We can also compare it
@@ -244,7 +247,7 @@ metrics$err[metrics$scen=="obs"] <- 0
 ## Redo scatterplot with uniform axes
 
 
-png(file="plot/corr-mae-unif.png", width=6, height=6, units="in", res=180)
+#png(file="plot/corr-mae-unif.png", width=6, height=6, units="in", res=180)
 par(mfcol=c(3,3), mar=c(3.1, 3.1, 2, 1), mgp=c(1.5,0.5,0))
 
 for (v in c("A850", "U850", "V850",
@@ -269,37 +272,102 @@ for (v in c("A850", "U850", "V850",
         }
     }
 }
-dev.off()
+#dev.off()
 
 
 ## Now redo metric and barplot.  Now that everything is on the same
 ## scale, I think we can just do Manhattan distance from the lower
 ## right corner.
 
-metrics$cred <- metrics$cor - metrics$err
+metrics$cred <- (metrics$cor - metrics$err + 2)/3
 
 cred <- xtabs(cred ~ gcm+method, data=metrics, subset=metrics$scen=="hist")
 cred <- cred[c("HadGEM","GFDL","MPI"),rev(methods[-1])] / 10
 
-barplot(cred, beside=TRUE, horiz=TRUE, las=1, xlim=c(-1.5,1),
-        col=c(HadGEM=2,GFDL=3,MPI=4), main="credibilty metric")
+dev.new()
+par(mfrow=c(1,2))
+barplot(cred, beside=TRUE, horiz=TRUE, las=1, xlim=c(0,1),
+        col=gcol, main="credibilty metric")
+
+barplot(t(cred), beside=TRUE, horiz=TRUE, las=1, xlim=c(0,1),
+        col=rep(gcol,each=ncol(cred)), main="credibilty metric")
 
 ## Okay!  I think that looks better.  However, I'm probaby
 ## overweighting winds, and I think I need to do proper vector
 ## correlations.
 
+library(fields)
+source("plotfun.R")
 
-## I think we need to do vector correlations for our winds.  So that
-## would be the inner product (geometry::dot()) of X1 & X2 divided by
-## the square roots of dot(X1,X1)*dot(X2,X2)
+zr <- c(-10,10)
 
-## And if I'm doing that for winds, I probably need to do MAE for
-## speed, not U/V
+mag <- function(x,y){sqrt(x^2+y^2)}
 
-## And then I don't think the rank ordering method for MAE is really
-## right because it ignores the magnitude and implicitly weights all
-## the variables the same, which I think may be not right.
+Uobs <- obs["U250",,]
+Vobs <- obs["V250",,]
 
-## Maybe just divide by max MAE for each field?
+Umod <- mod[9,"U250",,]
+Vmod <- mod[9,"V250",,]
 
-library(geometry)
+Uerr <- Umod - Uobs
+Verr <- Vmod - Vobs
+
+THobs <- atan2(Uobs, Vobs)
+THmod <- atan2(Umod, Vmod)
+
+UVcor <- cos(THmod - THobs)
+
+Mobs <- mag(Uobs, Vobs)
+Mmod <- mag(Umod, Vmod)
+Merr <- mag(Uerr, Verr)
+
+## hack to set uniform arrow scale
+Uobs[1,1] <- 10; Vobs[1,1] <- 0
+Umod[1,1] <- 0;  Vmod[1,1] <- 10
+Uerr[1,1] <- Vmod[1,1] <- -sqrt(10)
+
+par(mfrow=c(2,2))
+
+image.plot(dlon, dlat, mag(Uobs,Vobs), main="obs 250", zlim=zr)
+vectorfield(dlon, dlat, Uobs, Vobs, lwd=2, col=1)
+
+image.plot(dlon, dlat, mag(Umod,Vmod), main="mod 250", zlim=zr)
+vectorfield(dlon, dlat, Umod, Vmod, lwd=2, col=1)
+
+image.plot(dlon, dlat, mag(Uerr,Verr), main="err 250", zlim=zr)
+vectorfield(dlon, dlat, Uerr, Verr, lwd=2, col=1)
+
+image.plot(dlon, dlat, UVcor, main="cos theta 250", zlim=c(-1,1))
+
+## mean(UVcor) is the coefficient of correlation for a vector field
+## mean(Merr) is the MAE for the vector field
+
+## So that gives us:
+
+## U850, V850: vector corr, mean(merr)
+## U250, V250: vector corr, mean(merr)
+## T700: cor(), scaled MAE
+
+## Z700: cor(), scaled MAE
+## Q850: cor(), scaled MAE
+## A850: cor(), scaled MAE
+
+## A850 arguably overcounts moisture transport, but that's the most
+## important process here, so I think we should include it.
+
+## Of course, that also gives us 6 panels + legend, which is a pain.
+## So let's see if it makes a big difference...
+
+## S250: skip; covered by U250/V250
+## Z500: skip; highly correlated with Z700
+
+############################################
+
+## So do I update my scatter plots, or just the metrics barplot?
+
+## Update scatter.  Include Z500, and add a map of the region the
+## metrics are calculated over.
+
+## A850	T700	w250
+## w850	Z700	legend
+## Q850	Z500	map
